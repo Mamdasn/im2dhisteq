0df5ac87efdd0b924f030f899cf8f4c003d6ca70
mamdasn <mhsaber1375@gmail.com>
2021-04-16 04:18:27 +0430

    Update src

--- src/im2dhisteq.py
+++ src/im2dhisteq.py
@@ -1,34 +1,36 @@
 from im2dhist import im2dhist
 import numpy as np
+import numba
 
-
-def imhist(image):
-    hist, bins = np.histogram(image.reshape(1, -1), bins=256, range=(0, 255))
-    return (np.asarray(hist), np.asarray(bins[0:-1]))
+@numba.njit()
+def imhist(arr):
+    hist, _ = np.histogram(arr, bins=256, range=(0, 255))
+    return np.asarray(hist)
     
+@numba.njit()
 def im2dhisteq(image, w_neighboring=6, showProgress = True):
     [h, w] = image.shape
-    V = image
-    V_hist, _ = imhist(V)
+    V = image.copy()
+    V_hist = imhist(V.reshape(1, -1))
 
-    X = (V_hist > 0) * [i for i in range(1, 257)]
-    X = X[X>0]
-    K = len(X)
-
-    H_in = im2dhist(V, w_neighboring=6, showProgress = True)
+    H_in = im2dhist(V, w_neighboring=6)
 
     CDFx = np.cumsum(np.sum(H_in, axis=0)).reshape(-1, 1) # Kx1
 
     # normalizes CDFx
     CDFxn = (255*CDFx/CDFx[-1]).reshape(-1)
 
+    PDFxn = np.zeros_like(CDFxn)
+    PDFxn[0] = CDFxn[0]
+    PDFxn[1:] = np.diff(CDFxn)
+    
     X_transform = np.zeros((256))
-    X_transform[np.where(V_hist > 0)] = np.insert(np.diff(CDFxn), 0, CDFxn[0])
+    X_transform[np.where(V_hist > 0)] = PDFxn
     CDFxn_transform = np.cumsum(X_transform).reshape(-1)
 
 
     bins = np.array([i for i in range(0, 256)]).reshape(-1)
-    # use linear interpolation of cdf to find new pixel values
+    # uses linear interpolation of cdf to find new pixel values
     image_equalized = np.floor(np.interp(V.flatten(), bins, CDFxn_transform).reshape(h, w)).astype(np.uint8)
 
     return image_equalized
