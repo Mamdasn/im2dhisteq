b775605b59bb328c9a3dfd32e86d075a7910f872
mamdasn <mhsaber1375@gmail.com>
2021-04-16 04:18:27 +0430

    Update tests

--- tests/test_im2dhisteq.py
+++ tests/test_im2dhisteq.py
@@ -9,10 +9,10 @@ def test_im2dhisteq_with_param():
     # convert rgb image to gray
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    gray_image_2DHisteq = im2dhisteq(gray_image, w_neighboring=6, showProgress = True)
+    gray_image_2DHisteq = im2dhisteq(gray_image, w_neighboring=6)
     
 
     #np.save(f'{image_name}-2D-Histogram-Equalized', gray_image_2DHisteq)
     gray_image_2DHisteq_cmpr = np.load(f'{image_name}-2D-Histogram-Equalized.npy')
     assert np.all(gray_image_2DHisteq == gray_image_2DHisteq_cmpr)
-test_im2dhisteq_with_param()
+#test_im2dhisteq_with_param()
