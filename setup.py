1117b463cb3bbf507a961c4ffc816152a2c054d1
mamdasn <mhsaber1375@gmail.com>
2021-04-17 03:01:14 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='im2dhisteq',
-    version='0.0.3',
+    version='0.0.4',
     author="mamdasn s",
     author_email="<mamdassn@gmail.com>",
     url="https://github.com/Mamdasn/im2dhisteq",
