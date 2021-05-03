d7aa41694744a1e498f2e5dbede8b208da043c32
mamdasn <mhsaber1375@gmail.com>
2021-05-03 21:36:19 +0430

    Update repo

--- src/im2dhisteq.py
+++ src/im2dhisteq.py
@@ -2,29 +2,28 @@ from im2dhist import im2dhist, imhist
 import numpy as np
 import numba 
 
-# @numba.njit()
+@numba.njit()
 def im2dhisteq(image, w_neighboring=6):
-    [h, w] = image.shape
     V = image.copy()
+    [h, w] = V.shape
     V_hist = imhist(V)
+    H_in = im2dhist(V, w_neighboring=w_neighboring)
 
-    H_in = im2dhist(V, w_neighboring=6)
-
-    CDFx = np.cumsum(np.sum(H_in, axis=0)).reshape(-1, 1) # Kx1
+    CDFx = np.cumsum(np.sum(H_in, axis=0)) # Kx1
 
     # normalizes CDFx
-    CDFxn = (255*CDFx/CDFx[-1]).reshape(-1)
+    CDFxn = (255*CDFx/CDFx[-1])
 
     PDFxn = np.zeros_like(CDFxn)
     PDFxn[0] = CDFxn[0]
     PDFxn[1:] = np.diff(CDFxn)
-    
+
     X_transform = np.zeros((256))
-    X_transform[np.where(V_hist > 0)] = PDFxn
-    CDFxn_transform = np.cumsum(X_transform).reshape(-1)
+    X_transform[np.where(V_hist > 0)] = PDFxn.copy()
+    CDFxn_transform = np.cumsum(X_transform)
 
 
-    # bins = np.array([i for i in range(0, 256)]).reshape(-1)
+    # bins = np.array([i for i in range(0, 256)])
     bins = np.arange(256)
     # uses linear interpolation of cdf to find new pixel values
     image_equalized = np.floor(np.interp(V.flatten(), bins, CDFxn_transform).reshape(h, w)).astype(np.uint8)
@@ -39,18 +38,18 @@ def vid2dhisteq(image, w_neighboring=6, Wout_list=np.zeros((10))):
 
     H_in = im2dhist(V, w_neighboring=6)
 
-    CDFx = np.cumsum(np.sum(H_in, axis=0)).reshape(-1, 1) # Kx1
+    CDFx = np.cumsum(np.sum(H_in, axis=0)) # Kx1
 
     # normalizes CDFx
-    CDFxn = (CDFx/CDFx[-1]).reshape(-1)
+    CDFxn = (CDFx/CDFx[-1])
 
     PDFxn = np.zeros_like(CDFxn)
     PDFxn[0] = CDFxn[0]
     PDFxn[1:] = np.diff(CDFxn)
     
     X_transform = np.zeros((256))
-    X_transform[np.where(V_hist > 0)] = PDFxn
-    CDFxn_transform = np.cumsum(X_transform).copy().reshape(-1)
+    X_transform[np.where(V_hist > 0)] = PDFxn.copy()
+    CDFxn_transform = np.cumsum(X_transform).copy()
 
     Win = H_in.shape[0]
     Gmax = 1.5 # 1.5 .. 2
