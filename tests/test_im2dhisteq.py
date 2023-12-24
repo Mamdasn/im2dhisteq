2cc2a9cdcc4b307e84279f510aaa7f2ac4e16c49
Mamdasan S <mhsaber1375@gmail.com>
2023-12-25 00:54:51 +0100

    Change the address of the `test` image

--- tests/test_im2dhisteq.py
+++ tests/test_im2dhisteq.py
@@ -4,7 +4,7 @@ import cv2
 
 
 def test_im2dhisteq_with_param():
-    image_name = "../assets/Plane.jpg"
+    image_name = "assets/Plane.jpg"
     image = cv2.imread(image_name)
     # convert rgb image to gray
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -14,6 +14,3 @@ def test_im2dhisteq_with_param():
     # np.save(f'{image_name}-2D-Histogram-Equalized', gray_image_2DHisteq)
     gray_image_2DHisteq_cmpr = np.load(f"{image_name}-2D-Histogram-Equalized.npy")
     assert np.all(gray_image_2DHisteq == gray_image_2DHisteq_cmpr)
-
-
-# test_im2dhisteq_with_param()
