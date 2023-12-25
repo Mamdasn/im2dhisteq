0c93fb09169075b999123aa54829458775466874
Mamdasan S <mhsaber1375@gmail.com>
2023-12-25 02:26:01 +0100

    Add vid2dhisteq bin to scripts on `setup.py`
    + Increment package version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with codecs.open(os.path.join(parent_dir, "README.md"), encoding="utf-8") as rea
 
 setup(
     name="im2dhisteq",
-    version="1.0.1",
+    version="1.0.2",
     author="mamdasn s",
     author_email="<mamdassn@gmail.com>",
     url="https://github.com/Mamdasn/im2dhisteq",
@@ -45,5 +45,5 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
-    scripts=["bin/im2dhisteq"],
+    scripts=["bin/im2dhisteq", "bin/vid2dhisteq"],
 )
