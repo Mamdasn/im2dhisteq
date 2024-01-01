d81d7ede865116fde881bf51355e4e0d6ab5b9bf
Mamdasan S <mhsaber1375@gmail.com>
2024-01-01 18:25:05 +0100

    Import go libs if the go libs are compiled

--- src/go_libs/__init__.py
+++ src/go_libs/__init__.py
@@ -1,5 +1,7 @@
-from go_libs.go_libs import get_twodhist_file
-from go_libs.go_libs import get_twodhist
-from go_libs.go_libs import get_twodhist_parallel
-from go_libs.go_libs import get_imhist
 from go_libs.dll_path_finder import get_dll_path
+
+if get_dll_path():
+    from go_libs.go_libs import get_twodhist_file
+    from go_libs.go_libs import get_twodhist
+    from go_libs.go_libs import get_twodhist_parallel
+    from go_libs.go_libs import get_imhist
